326e493635c3c764a0302865b31d996120b63aef
keento0809 <handball0321@gmail.com>
2021-11-25 19:21:24 -0800

    Add an animation

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 window.addEventListener("DOMContentLoaded", () => {
+  // DOMの取得
   const body = document.querySelector("body");
   const menuBtn = document.querySelector(".mobile-menu-icon");
   const coverD = document.querySelector(".cover-dark");
+  const h2s = document.querySelectorAll("h2");
+  const h3s = document.querySelectorAll("h3");
+  const coverLogo = document.querySelector(".cover-logo");
+
+  const getHTML = (array) => {
+    array.forEach((arr) => {
+      arr.classList.toggle("slideIn");
+    });
+  };
+
   menuBtn.addEventListener("click", () => {
     coverD.classList.toggle("nom");
+    coverLogo.classList.toggle("slideIn");
+    getHTML(h2s);
+    getHTML(h3s);
   });
 });
