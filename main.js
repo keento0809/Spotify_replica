a810bcc0214c149e5263b262e4ae3c6c6519e5f1
KEENTO <handball0321@gmail.com>
2022-01-22 11:10:56 -0800

    Refactoring js file

--- main.js
+++ main.js
@@ -1,42 +1,39 @@
 window.addEventListener("DOMContentLoaded", () => {
-  // DOMの取得
-  const body = document.querySelector("body");
+  // Get DOM
+  // const body = document.querySelector("body");
   const menuBtn = document.querySelector(".mobile-menu-icon");
-  const additionalCover = document.querySelector(".additional-darkcover");
-  const coverD = document.querySelector(".cover-dark");
-  const coverDContainer = document.querySelector(".cover-dark-container");
+  const additionalDarkCover = document.querySelector(".additional-darkcover");
+  const darkCover = document.querySelector(".cover-dark");
+  // const darkCoverContainer = document.querySelector(".cover-dark-container");
   const h2s = document.querySelectorAll("h2");
   const h3s = document.querySelectorAll("h3");
   const coverLogo = document.querySelector(".cover-logo");
 
+  // Build out function
   const getHTML = (array) => {
     array.forEach((arr) => {
       arr.classList.toggle("slideIn");
     });
   };
 
-  // mobile menu用：addEventListenerのイベント発火条件を決める
-  // const getEventType = () => {
-  //   const isTouchCapable =
-  //     "ontouchstart" in window ||
-  //     (window.DocumentTouch &&
-  //       document instanceof
-  //         window.DocumentTouch.navigator.maxTouchPoints >
-  //         0) ||
-  //     window.navigator.msMaxTouchPoints > 0;
-  //   return isTouchCapable ? "touchstart" : "click";
-  // };
+  const slideIn = () => {
+    coverLogo.classList.toggle("slideIn");
+  };
 
-  // addEventListenerのイベント発火条件を定義
-  // const eventType = getEventType();
-  // console.log(coverDContainer);
+  const menuOpen = () => {
+    menuBtn.classList.toggle("menu-open");
+  };
 
+  const changeCover = () => {
+    darkCover.classList.toggle("nom");
+  };
+
+  // Hook up the event
   menuBtn.addEventListener("click", () => {
-    // coverDContainer.classList.toggle("nom");
     coverLogo.classList.toggle("slideIn");
     menuBtn.classList.toggle("menu-open");
-    coverD.classList.toggle("nom");
-    additionalCover.classList.toggle("nom-second");
+    darkCover.classList.toggle("nom");
+    additionalDarkCover.classList.toggle("nom-second");
     getHTML(h2s);
     getHTML(h3s);
   });
